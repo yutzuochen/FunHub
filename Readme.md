0cfa3bda16ed1ff7548676f5764be52eccc5e05b
yutzuochen <yutzuochen@example.com>
2023-01-24 17:16:48 +0800

    [add]go mod

---
+++ Readme.md
@@ -0,0 +1,12 @@
+## Package Structure
+
+```text
+FunHub
+ ├── client/              # 
+ ├── constant/         # 常數定義
+ ├── dao/              # 共用資料格式定義
+ ├── doc/              # FunHub 說明文件
+ ├── types/            # 資料類型定義
+ ├── util/             # 共用工具
+ └── main.go           # entry point
+```
--- server/server.go
+++ server/server.go
@@ -12,6 +12,11 @@ import (
 	"strings"
 	"syscall"
 	"time"
+
+	"gopkg.in/natefinch/lumberjack.v2"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var M = map[int]int{
@@ -23,7 +28,42 @@ var M = map[int]int{
 	6: 10,
 }
 
+var sugarLogger *zap.SugaredLogger
+
+func getEncoder() zapcore.Encoder {
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	encoderConfig.MessageKey = "message"
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	// encoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	return zapcore.NewConsoleEncoder(encoderConfig)
+}
+
+func getLogWriter() zapcore.WriteSyncer {
+	lumberJackLogger := &lumberjack.Logger{
+		Filename:   "./test.log",
+		MaxSize:    1,
+		MaxBackups: 5,
+		MaxAge:     30,
+		Compress:   false,
+	}
+	return zapcore.AddSync(lumberJackLogger)
+}
+
+func InitLogger() {
+	fmt.Println("InitLogger")
+	writeSyncer := getLogWriter()
+	encoder := getEncoder()
+	core := zapcore.NewCore(encoder, writeSyncer, zapcore.DebugLevel)
+
+	logger := zap.New(core, zap.AddCaller())
+	sugarLogger = logger.Sugar()
+	sugarLogger.Debug("Initlogger")
+}
 func main() {
+	InitLogger()
+	defer sugarLogger.Sync()
 	ctx := context.Background()
 	mux := http.NewServeMux()
 	mux.HandleFunc("/play/", play)
@@ -34,12 +74,13 @@ func main() {
 		Handler:      mux,
 	}
 
-	log.Println("starting httpserver at http:localhost:1210")
+	sugarLogger.Info("starting httpserver at http:localhost:1210")
+	sugarLogger.Debug("starting httpserver at http:localhost:1210_debug")
 
 	go func() {
 		// service connections
 		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("listen: %s\n", err)
+			panic(err)
 		}
 	}()
 
@@ -48,16 +89,16 @@ func main() {
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	//阻塞channel
 	s := <-quit
-	log.Println("Got signal: ", s)
+	sugarLogger.Info("Got signal: ", s)
 	// If process haven't finished within 10second, we will force to shutdown the process.'
 	c, cancel := context.WithTimeout(ctx, 10*time.Second)
 	defer cancel()
-	log.Println("Graceful Shutdown start ")
+	sugarLogger.Info("Graceful Shutdown start ")
 
 	if err := server.Shutdown(c); err != nil {
-		log.Println("server.Shutdown:", err)
+		sugarLogger.Error("server.Shutdown:", err)
 	}
-	log.Println("Graceful Shutdown end ")
+	sugarLogger.Info("Graceful Shutdown end ")
 }
 func randomInt(min, max int) int {
 	return min + rand.Intn(max-min)
@@ -67,7 +108,7 @@ func play(w http.ResponseWriter, r *http.Request) {
 	bet_str := strings.TrimPrefix(r.URL.Path, "/play/")
 	rand.Seed(time.Now().UnixNano())
 	ran := randomInt(1, 7)
-	log.Println("bet, multiple, random number: ", bet_str, M[ran], ran)
+	sugarLogger.Debug("bet, multiple, random number: ", bet_str, M[ran], ran)
 	bet, err := strconv.Atoi(bet_str)
 	if err != nil {
 		log.Fatal(err)
